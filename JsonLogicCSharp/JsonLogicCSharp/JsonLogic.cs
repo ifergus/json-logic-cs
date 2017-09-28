4423abb25d9a2002625220ddbd8d7d643f7f4491
Ivor Fergus <ivor.fergus@gmail.com>
2017-09-27 23:32:27 -0400

    Initial Commit

---
+++ JsonLogicCSharp/JsonLogicCSharp/JsonLogic.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonLogicCSharp
+{
+    public static class JsonLogic
+    {
+
+        static class OperationHandler
+        {
+
+            private static Dictionary<(int, string), Func<JArray, IEnumerable<dynamic>, JToken, dynamic>> Operations = null;
+
+            static OperationHandler()
+            {
+                Operations = new Dictionary<(int, string), Func<JArray, IEnumerable<dynamic>, JToken, dynamic>>()
+                {
+
+                    [(1, "missing")] = (v, p, d) =>
+                    {
+                        var v1 = v.Select(x => x);
+                        var d1 = d.Select(x => x.Path);
+                        return new JArray(v1.Where(s => !d1.Any(t => t.ToString() == s.ToString())));
+                    },
+
+                    [(1, "missing_some")] = (v, p, d) => (Operations[(1, "missing")](v, p, d) as JArray).Count > 0,
+
+                    [(1, "and")] = (v, p, d) => !v.Any(x => !Truthy(ApplyInternal(x, d))),
+
+                    [(1, "or")] = (v, p, d) => v.Any(x => Truthy(ApplyInternal(x, d))),
+
+                    [(1, "in")] = (v, p, d) =>
+                    {
+                        if (v.Where(x => x is JArray).Count() > 0)
+                        {
+                            return v.Last.Select(x => x).Contains(v.First);
+                        }
+
+                        return ((string)v.Last).Contains((string)v.First);
+                    },
+
+                    [(1, "if")] = (v, p, d) =>
+                    {
+                        int i;
+                        for (i = 0; i < v.Count - 1; i += 2)
+                        {
+                            if (Truthy(ApplyInternal(v[i], d)))
+                            {
+                                return ApplyInternal(v[i + 1], d);
+                            }
+                        }
+
+                        if (v.Count == i + 1)
+                        {
+                            return ApplyInternal(v[i], d);
+                        }
+
+                        return null;
+                    },
+
+                    [(1, "?:")] = (v, p, d) => Operations[(1, "if")](v, p, d),
+
+                    [(1, "var")] = (v, p, d) =>
+                    {
+                        if (v.First.Type == JTokenType.String)
+                        {
+                            return d.SelectToken((string)v.First);
+                        }
+                        else if (v.First.Type == JTokenType.Integer)
+                        {
+                            var index = (int)v.First;
+                            return (d.First.Last as JArray)[index];
+                        }
+
+                        return null;
+                    },
+
+
+                    [(1, "merge")] = (v, p, d) => new JArray(v.SelectMany(x => x).Select(y => (y as JValue).Value)),
+
+                    [(1, "cat")] = (v, p, d) => v.Aggregate((i, j) => i + "" + j),
+
+                    [(1, "log")] = (v, p, d) =>
+                    {
+                        Console.WriteLine(v.First);
+                        return v;
+                    },
+
+                    [(1, "!")] = (v, p, d) => !Truthy(v.First),
+
+                    [(1, "!!")] = (v, p, d) => Truthy(v.First),
+
+                    [(2, "==")] = (v, p, d) => p.First() == p.Last(),
+
+                    [(2, "===")] = (v, p, d) => p.First() == p.Last() && p.First().GetHashCode() == p.Last().GetHashCode(),
+
+                    [(2, "!=")] = (v, p, d) => p.First() != p.Last(),
+
+                    [(2, ">")] = (v, p, d) => !p.Zip(p.Skip(1), (a, b) => a > b).Contains(false),
+
+                    [(2, ">=")] = (v, p, d) => p.First() >= p.Last(),
+
+                    [(2, "<")] = (v, p, d) => !p.Zip(p.Skip(1), (a, b) => a < b).Contains(false),
+
+                    [(2, "<=")] = (v, p, d) => p.First() <= p.Last(),
+
+                    [(2, "%")] = (v, p, d) => p.First() % p.Last(),
+
+                    [(2, "/")] = (v, p, d) => p.First() / p.Last(),
+
+                    [(2, "-")] = (v, p, d) => p.Count() == 1 ? p.First() * -1 : p.First() - p.Last(),
+
+                    [(2, "*")] = (v, p, d) => p.Aggregate((a, b) => a * b),
+
+                    [(2, "+")] = (v, p, d) => p.Sum(a => a),
+
+                    [(2, "min")] = (v, p, d) => p.Min(),
+
+                    [(2, "max")] = (v, p, d) => p.Max()
+
+                };
+            }
+
+            public static dynamic PerformOperation(string opSymbol, int order, JArray values, JToken data) => Operations[(order, opSymbol)](values, values, data);
+
+            public static bool IsOperationCode(string opSymbol) => Operations.ContainsKey((1, opSymbol)) || Operations.ContainsKey((2, opSymbol));
+
+            public static bool IsOperationInvokable(string opSymbol, int order) => Operations.ContainsKey((order, opSymbol));
+
+        };
+
+
+        public static dynamic Apply(string jsonExpression, string jsonData = null)
+        {
+            JObject expressionObj = JsonConvert.DeserializeObject<JObject>(jsonExpression);
+            JObject dataObj = (jsonData == null) ? null : JsonConvert.DeserializeObject<JObject>(jsonData);
+
+            var jsonResult = ApplyInternal(expressionObj.First, dataObj);
+
+            // convert result to standard .Net type, if possible. 
+            var dotNetResult = ConvertToDotNetType(jsonResult);
+
+            return dotNetResult;
+        }
+
+
+        public static bool Truthy(JToken token)
+        {
+            switch (token)
+            {
+                case null:
+
+                    return false;
+
+                case JValue val when val.Type == JTokenType.String:
+
+                    return !String.IsNullOrEmpty((string)val);
+
+                case JValue val when val.Type == JTokenType.Integer:
+
+                    return (int)val != 0;
+
+                case JValue val when val.Type == JTokenType.Float:
+
+                    return (double)val != 0.0d;
+
+                case JValue val when val.Type == JTokenType.Boolean:
+
+                    return (bool)val;
+
+                case JArray array:
+
+                    return array.HasValues;
+
+                case JObject obj:
+
+                    return obj.HasValues;
+
+                default:
+
+                    return false;
+            }
+        }
+
+        private static bool IsLogicalExpression(JToken token) => (token is JProperty prop) && prop != null && OperationHandler.IsOperationCode(prop.Name);
+
+
+
+        private static string ExtractOpSymbol(JToken token)
+        {
+            if (token is JProperty prop)
+            {
+                return prop.Name;
+            }
+
+            throw new InvalidOperationException("Unable to extract Op symbol");
+        }
+
+        private static JArray ExtractValues(JToken token)
+        {
+
+            if (token == null)
+            {
+                return new JArray();
+
+            }
+            else if (token.First is JArray array)
+            {
+                return array;
+            }
+
+            return new JArray() { token.First };
+        }
+
+
+        private static dynamic ApplyInternal(JToken logic, JToken data)
+        {
+            if (logic is JArray array)
+            {
+                return new JArray() { array.Select(x => ApplyInternal(x, data)) };
+            }
+
+            if (logic is JObject obj)
+            {
+                logic = obj.Properties().First();
+            }
+
+            if (!IsLogicalExpression(logic))
+            {
+                return logic;
+            }
+
+            if (!Truthy(data))
+            {
+                data = new JArray();
+            }
+
+            var opSymbol = ExtractOpSymbol(logic);
+            var values = ExtractValues(logic);
+
+
+            if (OperationHandler.IsOperationInvokable(opSymbol, 1))
+            {
+                return OperationHandler.PerformOperation(opSymbol, 1, values, data);
+            }
+
+            values = new JArray() { values.Select(x => ApplyInternal(x, data)).ToArray() };
+
+            if (OperationHandler.IsOperationInvokable(opSymbol, 2))
+            {
+                return OperationHandler.PerformOperation(opSymbol, 2, values, data);
+            }
+
+            throw new KeyNotFoundException("Operator not defined: " + opSymbol);
+        }
+
+
+        public static dynamic ConvertToDotNetType(dynamic token)
+        {
+
+            switch (token)
+            {
+                case null:
+
+                    return null;
+
+                case JValue val when val.Type == JTokenType.String:
+
+                    return (string)val;
+
+                case JValue val when val.Type == JTokenType.Integer:
+
+                    return (int)val;
+
+                case JValue val when val.Type == JTokenType.Float:
+
+                    return (double)val;
+
+                case JValue val when val.Type == JTokenType.Boolean:
+
+                    return (bool)val;
+
+                case JArray array:
+
+                    try
+                    {
+                        if (array.Count > 0)
+                        {
+                            var arrayType = array[0].Type;
+
+                            switch (arrayType)
+                            {
+                                case JTokenType.Integer:
+                                    return array.Select(x => (int)x).ToArray(); ;
+                                case JTokenType.Float:
+                                    return array.Select(x => (double)x).ToArray(); ;
+                                case JTokenType.String:
+                                    return array.Select(x => (string)x).ToArray(); ;
+                                case JTokenType.Boolean:
+                                    return array.Select(x => (bool)x).ToArray();
+                                default:
+                                    return array;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        // do nothing;
+                    }
+
+                    return array;
+
+                case JObject obj:
+
+                    return obj;
+
+                default:
+
+                    return token;
+            }
+        }
+    }
+}
---
+++ JsonLogicCSharp/UnitTests/UnitTest1.cs
@@ -0,0 +1,291 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonLogicCSharp;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OpTestDeepEqual()
+        {
+            Assert.IsFalse(JsonLogic.Apply("{\"===\" :[2, 2.0]}"));
+            Assert.IsTrue(JsonLogic.Apply("{\"===\" :[2.0, 2.0]}"));
+            Assert.IsTrue(JsonLogic.Apply("{\"===\" :[\"dog\", \"dog\"]}"));
+        }
+
+        [TestMethod]
+        public void OpTestShallowEqual()
+        {
+            Assert.IsTrue(JsonLogic.Apply("{\"==\" :[2, 2.0]}"));
+            Assert.IsTrue(JsonLogic.Apply("{\"==\" :[2.0, 2.0]}"));
+            Assert.IsFalse(JsonLogic.Apply("{\"==\" :[2.0, 3.0]}"));
+            Assert.IsTrue(JsonLogic.Apply("{\"==\" :[\"dog\", \"dog\"]}"));
+
+            Assert.IsTrue(JsonLogic.Apply("{'==' :[2, 2.0]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'==' :[2.0, 2.0]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'==' :[2.0, 3.0]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'==' :['dog', 'dog']}"));
+        }
+
+        [TestMethod]
+        public void OpTestNot01()
+        {
+            Assert.IsTrue(JsonLogic.Apply("{\"!\" :false}"));
+            Assert.IsFalse(JsonLogic.Apply("{\"!\" :true}"));
+            Assert.IsFalse(JsonLogic.Apply("{\"!!\" :false}"));
+            Assert.IsTrue(JsonLogic.Apply("{\"!!\" :true}"));
+        }
+
+        [TestMethod]
+        public void OpTestNot02()
+        {
+            Assert.IsTrue(JsonLogic.Apply("{\"!\" :[false]}"));
+            Assert.IsFalse(JsonLogic.Apply("{\"!\" :[true]}"));
+            Assert.IsFalse(JsonLogic.Apply("{\"!!\" :[false]}"));
+            Assert.IsTrue(JsonLogic.Apply("{\"!!\" :[true]}"));
+        }
+
+        [TestMethod]
+        public void OpTestAdd01()
+        {
+            Assert.AreEqual(5, JsonLogic.Apply("{\"+\" :[2, 3]}"));
+            Assert.AreEqual(4.0, JsonLogic.Apply("{\"+\" :[2.0, 2.0]}"));
+            Assert.AreEqual(8, JsonLogic.Apply("{\"+\" :[2, 2, 2, 2]}"));
+            Assert.AreEqual(10, JsonLogic.Apply("{\"+\" :[10]}"));
+            Assert.AreEqual(10, JsonLogic.Apply("{\"+\" :10}"));
+        }
+
+        [TestMethod]
+        public void OpTestMult01()
+        {
+            Assert.AreEqual(6, JsonLogic.Apply("{'*' :[2, 3]}"));
+            Assert.AreEqual(9.0, JsonLogic.Apply("{\"*\" :[3.0, 3.0]}"));
+            Assert.AreEqual(16, JsonLogic.Apply("{\"*\" :[2, 2, 2, 2]}"));
+            Assert.AreEqual(10, JsonLogic.Apply("{\"*\" :[10]}"));
+            Assert.AreEqual(10, JsonLogic.Apply("{\"*\" :10}"));
+        }
+
+        [TestMethod]
+        public void OpTestDiv01()
+        {
+            Assert.AreEqual(6, JsonLogic.Apply("{'/' :[12, 2]}"));
+            Assert.AreEqual(2.5, JsonLogic.Apply("{'/' :[5.0, 2.0]}"));
+            Assert.AreEqual(2, JsonLogic.Apply("{'/' :[5, 2]}"));
+        }
+
+        [TestMethod]
+        public void OpTestLessThan01()
+        {
+            Assert.IsTrue(JsonLogic.Apply("{'<' :[2, 3]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'<' :[3, 2]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'<' :[1, 2, 3, 4, 5]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'<' :[2, 3, 4, 1]}"));
+        }
+
+        [TestMethod]
+        public void OpTestGreaterThan01()
+        {
+            Assert.IsFalse(JsonLogic.Apply("{'>' :[2, 3]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'>' :[3, 2]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'>' :[1, 2, 3, 4, 5]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'>' :[5, 4, 3, 2, 1]}"));
+        }
+
+        [TestMethod]
+        public void OpTestComplexAdd01()
+        {
+            Assert.AreEqual(27, JsonLogic.Apply(
+                            @"{'+': 
+                                [   { '*' : [3,2]},
+                                    { '+' : [2,2,2,2,2]},
+                                    { '+' : [2,2,2,2,2]},
+                                    { '/' : [10,10]}
+                                ]
+                              }"));
+        }
+
+        [TestMethod]
+        public void OpTestBoolean01()
+        {
+            Assert.IsFalse(JsonLogic.Apply("{'and': [true, true, true, false]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'and': [true, true, true, true]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'or': [false, false, false, true]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'or': [false, false, false, false]}"));
+        }
+
+        [TestMethod]
+        public void OpTestMin01()
+        {
+            Assert.AreEqual(1, JsonLogic.Apply("{'min': [1,2,3]}"));
+            Assert.AreEqual(8, JsonLogic.Apply("{'min': [10,9,8]}"));
+            Assert.AreEqual(8.1, JsonLogic.Apply("{'min': [10.1,9.1,8.1]}"));
+        }
+
+        [TestMethod]
+
+        public void OpTestMax01()
+        {
+            Assert.AreEqual(3, JsonLogic.Apply("{'max': [1,2,3]}"));
+            Assert.AreEqual(10, JsonLogic.Apply("{'max': [10,9,8]}"));
+            Assert.AreEqual(10.1, JsonLogic.Apply("{'max': [10.1,9.1,8.1]}"));
+        }
+
+        [TestMethod]
+        public void OpTestIf01()
+        {
+            Assert.AreEqual("yes", JsonLogic.Apply("{'if' : [true, 'yes', 'no']}"));
+            Assert.AreEqual("no", JsonLogic.Apply("{'if' : [false, 'yes', 'no']}"));
+        }
+
+        [TestMethod]
+        public void OpTestIn01()
+        {
+            Assert.IsTrue(JsonLogic.Apply("{'in':[ 'Ringo', ['John', 'Paul', 'George', 'Ringo'] ]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'in':[ 'Ringo', ['John', 'Paul', 'George'] ]}"));
+            Assert.IsTrue(JsonLogic.Apply("{'in':[ 1, [2, 3, 4, 1] ]}"));
+            Assert.IsFalse(JsonLogic.Apply("{'in':[ 1, [2, 3, 4] ]}"));
+        }
+
+        [TestMethod]
+        public void OpTestIn02()
+        {
+            Assert.IsTrue(JsonLogic.Apply("{'in':[ 'Spring', 'Springfield']}"));
+            Assert.IsFalse(JsonLogic.Apply("{'in':[ 'Springx', 'Springfield']}"));
+        }
+
+        [TestMethod]
+        public void OpTestBoolean02()
+        {
+            Assert.IsTrue(JsonLogic.Apply(
+                            @"{'and': 
+                                [   { '==' : [3,3]},
+                                    { '==' : [2,2]},
+                                    { '!=' : [4,2]}                                   
+                                ]
+                              }"));
+
+            Assert.IsFalse(JsonLogic.Apply(
+                           @"{'and': 
+                                [   { '==' : [3,3]},
+                                    { '==' : [2,2]},
+                                    { '==' : [4,2]}                                   
+                                ]
+                              }"));
+        }
+
+        [TestMethod]
+        public void OpTestVar01()
+        {
+            string rule = @"{'var': 'a'}";
+            string data = @"{'a' : 1}";
+
+            Assert.AreEqual(1, JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestVar02()
+        {
+            string rule = @"{'var': 'person1.age'}";
+            string data = @"{'person1' : {'name': 'Mary', 'age': 10, },
+                             'person2' : {'name': 'Joe', 'age': 15, }}";
+
+            Assert.AreEqual(10, JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestVar03()
+        {
+            string rule = @"{'+': [1, {'var':'age'}, 2]}";
+            string data = @"{'age' : 10, 'salary' : 150000}";
+
+            Assert.AreEqual(13, JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestVar04()
+        {
+            string rule = @"{'>': [{'var':'age'}, 30]}";
+            string data = @"{'age' : 25, 'salary' : 150000}";
+
+            Assert.IsFalse(JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestVar05()
+        {
+            string rule = @"{'<': [{'var':'age'}, 30]}";
+            string data = @"{'age' : 25, 'salary' : 150000}";
+
+            Assert.IsTrue(JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestVar06()
+        {
+            string rule = @"{'var': 1}";
+            string data = @"{'x': ['zero', 'one', 'two', 'three']}";
+
+            Assert.AreEqual("one", JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestMissing01()
+        {
+            string rule = @"{'missing': ['a', 'b', 'c']}";
+            string data = @"{'a':'ape', 'b':'boy'}";
+
+            CollectionAssert.AreEqual(new string[] { "c" }, JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestMissing02()
+        {
+            string rule = @"{'missing': ['a', 'b', 'c']}";
+            string data = @"{'a':'ape', 'b':'boy', 'c':'cat'}";
+
+            CollectionAssert.AreEqual(new string[] { }, JsonLogic.Apply(rule, data));
+        }
+
+
+
+        [TestMethod]
+        public void OpTestSomeMissing01()
+        {
+            string rule = @"{'missing_some': ['a', 'b', 'c']}";
+            string data = @"{'a':'ape', 'b':'boy', 'c':'cat'}";
+
+            Assert.AreEqual(false, JsonLogic.Apply(rule, data));
+
+            rule = @"{'missing_some': ['a', 'b', 'c']}";
+            data = @"{'a':'ape', 'b':'boy'}";
+
+            Assert.IsTrue(JsonLogic.Apply(rule, data));
+        }
+
+        [TestMethod]
+        public void OpTestMerge01()
+        {
+            string rule = @"{'merge' :[ [1,2], [3,4] ]}";
+
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4 }, JsonLogic.Apply(rule, null));
+        }
+
+        [TestMethod]
+        public void OpTestMerge02()
+        {
+            string rule = @"{'merge' :[ ['a', 'b'], ['c','d'] ]}";
+
+            CollectionAssert.AreEqual(new string[] { "a", "b", "c", "d" }, JsonLogic.Apply(rule, null));
+        }
+
+
+        [TestMethod]
+        public void OpTestCat01()
+        {
+            string rule = @"{'cat' : ['The ', 'cat ', 'in ', 'the ', 'hat.']}";
+
+            Assert.AreEqual("The cat in the hat.", JsonLogic.Apply(rule, null));
+        }
+    }
+}
